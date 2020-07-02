f24b3530017bc51ee7819d79da78e62e41be8cd7
Mohammadreza <mohammadreza.abdoli@digikala.com>
2020-07-02 18:01:44 +0430

    feat: add projecteuler fir challange

---
+++ projecteuler/firstChallange.js
@@ -0,0 +1,10 @@
+
+function multiple3or5(){
+    let a = 0;
+        for(let i = 0; i < 1000; i++){
+            if(i % 3 === 0 || i % 5 === 0){
+                a += i;
+            }
+        }
+    return a;
+}
